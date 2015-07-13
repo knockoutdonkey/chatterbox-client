ec162f88575bbb19006a07444bfd0051cee4e41d
Hack Reactor Students <accounts@hackreactor.com>
2015-07-13 16:00:26 -0700

    Now displays all recently posted messagese

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,30 +1,42 @@
 // YOUR CODE HERE:
 
 var message = {
-  username: 'knockoutdonkey',
-  text: 'I\'m down to clown!',
-  roomname: 'koDonkeyWorld'
+  username: 'KODonkey',
+  text: 'welcome to donkeyWorld',
+  roomname: 'KODonkeyWorld',
 };
 
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'https://api.parse.com/1/classes/chatterbox',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message');
-  }
-});
+// $.ajax({
+//   // This is the url you should use to communicate with the parse API server.
+//   url: 'https://api.parse.com/1/classes/chatterbox',
+//   type: 'POST',
+//   data: JSON.stringify(message),
+//   contentType: 'application/json',
+//   success: function (data) {
+//     console.log('chatterbox: Message sent');
+//   },
+//   error: function (data) {
+//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//     console.error('chatterbox: Failed to send message');
+//   }
+// });
+// function encode_utf8(s) {
+//   return unescape(encodeURIComponent(s));
+// }
 
+var responseHandler = function(data) {
+  // $ make it not json (data);
+  var messages = data.results; 
+  console.log(messages[0]);
+  messages.forEach(function(post) {
+    $("body").append('<p>' + post.text + '</p>');
+  })
+
+};
+var testData = JSON.stringify('where={"roomname"="KODonkeyWorld"}');
 $.ajax({
-  url: 'https://api.parse.com/1/classes/chatterbox',
+  url: 'https://api.parse.com/1/classes/chatterbox?where=' + encodeURIComponent('{"roomname":"koDonkeyWorld"}'),
   type: 'GET',
-  success: function (data) {
-    console.log(data.results[0].text);
-  }
+  contentType: 'application/json',
+  success: responseHandler
 });
